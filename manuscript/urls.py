8bc99e552461ddb634104fa2f7a5be0910c2b32a
bl1nkker <nurovich14@gmail.com>
2023-01-04 20:09:08 +0600

    fix: added urls

--- manuscript/urls.py
+++ manuscript/urls.py
@@ -1,4 +1,5 @@
 import app.views as views
+import app.views_ticket as views_ticket
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
@@ -14,8 +15,12 @@ urlpatterns = [
     path('register/', views.RegisterUserView.as_view(), name='register_user'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('events/', views.CreateListEvents.as_view(), name='create_list_events'),
+    path('tickets/', views_ticket.CreateListTicketView.as_view(),
+         name='create_list_tickets'),
     path('events/<int:pk>/', views.RetrieveUpdateDeleteEvent.as_view(),
-         name='retrieve_update_delete_event')
+         name='retrieve_update_delete_event'),
+    path('tickets/<int:pk>/', views_ticket.RetrieveUpdateDeleteTicketView.as_view(),
+         name='retrieve_update_delete_ticket')
 ]
 
 if settings.DEBUG:
