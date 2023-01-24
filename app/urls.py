09f28269a338364122a4999d00a359839312360f
bl1nkker <nurovich14@gmail.com>
2023-01-24 18:35:21 +0600

    feat: added login and register page

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 import app.views as views
 urlpatterns = [
-    path('login', views.login, name='login'),
+    path('signin', views.signin, name='signin'),
+    path('signup', views.signup, name='signup'),
 ]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,33 @@
 from django.shortcuts import render
-
+import json
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
 # Create your views here.
 
 
-def login(request):
-    return render(request, 'login.html')
+def signin(request):
+    if request.method == "POST":
+
+        email = request.POST.get("email", "").lower()
+        password = request.POST.get("password", "")
+        user = authenticate(request, username=email, password=password)
+        if user is not None:
+            # Проверка, что пользователь существует, но студент или преподаватель не созданы
+            # Это означает, что пользователь еще не прошел верификацию по почте
+            # if not (
+            #         Student.objects.filter(user=user).exists()
+            #         or Teacher.objects.filter(user=user).exists()
+            #         or Assistant.objects.filter(user=user).exists()
+            # ):
+            #     return HttpResponse(content=json.dumps({"user_id": user.id}), status=401)
+            login(request, user)
+            return redirect("/")
+        return HttpResponse(status=402)
+    return render(request, 'auth/signin.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        pass
+    return render(request, 'auth/signup.html')
