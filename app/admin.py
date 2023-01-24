87ddef08c0c302a08aed2aa4ea896d809b76cdb9
bl1nkker <nurovich14@gmail.com>
2023-01-24 17:44:29 +0600

    feat: added app and api projects

--- app/admin.py
+++ app/admin.py
@@ -1,7 +1,3 @@
 from django.contrib import admin
-from app.models import User, EventType, Event, Ticket
+
 # Register your models here.
-admin.site.register(User)
-admin.site.register(EventType)
-admin.site.register(Event)
-admin.site.register(Ticket)
